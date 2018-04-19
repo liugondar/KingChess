3c2ea8898ca630961f78cf67aab192c4990c7e0a
hungvu97 <16520493@gm.uit.edu.vn>
2018-04-19 21:33:43 +0700

    Init common and chess square

---
+++ ChessKing/ChessSquare.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessKing
+{
+    class ChessSquare:Button
+    {
+        enum ColorTeam
+        {
+            None,
+            White,
+            Black,
+        };
+        public Chess Chess { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public ChessSquare(ChessSquare a)
+        {
+            Chess = a.Chess;
+            Image = a.Image;
+            Row = a.Row;
+            Col = a.Col;
+        }
+
+    }
+}
---
+++ ChessKing/Common.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessKing
+{
+    class Common
+    {
+        static public bool Is2PlayerMode = false; //1 player
+        static public bool IsSelectedSquare = false; //selected yet
+        static public int IsTurn = 0;
+        static public bool Close = false;
+
+        static public ChessSquare[,] Board;
+        static public List<ChessSquare> CanMove = new List<ChessSquare>(); // create list, save position of piece can move
+        static public List<ChessSquare> CanEat = new List<ChessSquare>();
+
+        static public int RowSelected = -1; //set value =-1, not in chessboard
+        static public int ColSelected = -1; //set value =-1, not in chessboard
+
+        static public Color OldBackGround;//keep back ground before change to violet
+
+        static public bool CheckPromote = false; //phong hau
+        static public int RowProQueen = -1;
+        static public int ColProQueen = -1;
+
+        static public int Depth = 2;
+
+    }
+}
